c026a533a268037076d12a35fad1cb9cb10364cf
Denis Shtabnoy <tr0ythex@gmail.com>
2015-12-25 12:42:27 +0000

    Tests sending emails

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,13 +1,7 @@
 class Notifier < ApplicationMailer
   default :from => "noreply@xmerch.ru"
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.notifier.send_email.subject
-  #
-  def send_email
-    @greeting = "Hi"
 
+  def send_email
     mail to: "sales@xmerch.ru", :subject => "FUCK YOU!!!"
   end
 end
