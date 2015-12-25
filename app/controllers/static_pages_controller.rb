c08e0bd69bc68cd3e1c3bab3b3206b0df1393030
Denis Shtabnoy <tr0ythex@gmail.com>
2015-12-25 15:39:50 +0000

    Finishes mail sending from callback form. Adds html5 validations for this form.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,4 @@
 class StaticPagesController < ApplicationController
-  # def send_email
-  #   @contact = Contact.new(name: params[:name], email: params[:email], phone: params[:phone])
-  #   @contact.request = request
-  #   if @contact.deliver
-  #     flash[:notice] = 'Thank you for your message. We will contact you soon!'
-  #     redirect_to root_path
-  #   else
-  #     flash[:error] = 'Cannot send message.'
-  #     redirect_to root_path
-  #     # render :new
-  #   end
-  # end
   def send_email
     if Notifier.send_email(params[:name], params[:phone], params[:email]).deliver
       flash[:notice] = 'Thank you for your message. We will contact you soon!'
