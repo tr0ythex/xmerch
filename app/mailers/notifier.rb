0b46ce68211265c3a323075bcb64745c87f503f5
Denis Shtabnoy <tr0ythex@gmail.com>
2015-12-25 12:34:56 +0000

    Tests sending emails

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,5 +1,5 @@
 class Notifier < ApplicationMailer
-  default :from => "notifier"
+  default :from => "noreply@xmerch.ru"
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
