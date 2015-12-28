931c6012bf429a450856fbe20376df1fb69d0b55
Denis Shtabnoy <tr0ythex@gmail.com>
2015-12-28 09:13:34 +0000

    Changes CSRF to null_session

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,15 @@
 class StaticPagesController < ApplicationController
+  # def send_email
+  #   if Notifier.send_email(params[:name], params[:phone], params[:email]).deliver
+  #     flash[:notice] = 'Thank you for your message. We will contact you soon!'
+  #     redirect_to root_path
+  #   else
+  #     flash[:error] = 'Cannot send message.'
+  #     redirect_to root_path
+  #   end
+  # end
   def send_email
-    if Notifier.send_email(params[:name], params[:phone], params[:email]).deliver
-      flash[:notice] = 'Thank you for your message. We will contact you soon!'
-      redirect_to root_path
-    else
-      flash[:error] = 'Cannot send message.'
-      redirect_to root_path
-    end
+    flash[:notice] = 'Thank you for your message. We will contact you soon!'
+    redirect_to root_path
   end
 end
