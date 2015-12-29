87d8562247dbe338c0789b766c436b2f74c8608a
Denis Shtabnoy <tr0ythex@gmail.com>
2015-12-29 15:40:01 +0000

    Adds ajax to callback form section for success and error scenario

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,13 @@
 class StaticPagesController < ApplicationController
-  # def send_email
-  #   if Notifier.send_email(params[:name], params[:phone], params[:email]).deliver
-  #     flash[:notice] = 'Thank you for your message. We will contact you soon!'
-  #     redirect_to root_path
-  #   else
-  #     flash[:error] = 'Cannot send message.'
-  #     redirect_to root_path
-  #   end
-  # end
   def send_email
-    flash[:notice] = 'Thank you for your message. We will contact you soon!'
-    redirect_to root_path
+    if Notifier.send_email(params[:name], params[:phone], params[:email]).deliver
+      respond_to do |format|
+        format.js { render 'form_success' }
+      end
+    else
+      respond_to do |format|
+        format.js { render 'form_error' }
+      end
+    end
   end
 end
